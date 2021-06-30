fb49687b08cf55edbbf3f0a344816b737a4f39cb
Darshan Patel <darshanpatel@splunk.com>
2021-06-30 06:06:00 +0000

    MISP: Bug fixes as per PAPP-17068

---
+++ misp_consts.py
@@ -0,0 +1,12 @@
+# File: misp_consts.py
+# Copyright (c) 2017-2021 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+#
+
+MISP_INVALID_INT_ERR = "Please provide a valid {msg} integer value in the {param}"
+MISP_INVALID_EVENT_ID = "'event_id' action parameter"
+MISP_INVALID_MAX_RESULT = "'max_result' action parameter"
+MISP_ERR_CODE_MESSAGE = "Error code unavailable"
+MISP_ERR_MESSAGE = "Unknown error occurred. Please check the asset configuration and|or action parameters."
